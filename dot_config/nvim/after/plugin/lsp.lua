cabc7f6448ce7c424ca6a810211851f1b1de829f
Ben Stuart <ben@benstuart.ie>
2023-08-01 02:09:42 +0100

    Configure LSPs

--- dot_config/nvim/after/plugin/lsp.lua
+++ dot_config/nvim/after/plugin/lsp.lua
@@ -1,10 +1,42 @@
 local lsp = require('lsp-zero').preset({})
 
 lsp.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp.default_keymaps({buffer = bufnr})
+    -- see :help lsp-zero-keybindings
+    -- to learn the available actions
+    lsp.default_keymaps({buffer = bufnr})
 end)
+
+require("mason").setup()
+require("mason-lspconfig").setup({
+    ensure_installed = {
+        -- Lua
+        "lua_ls",
+        -- Bash
+        "bashls",
+        -- Docker
+        "dockerls",
+        -- Go
+        "gopls",
+        -- Data files
+        "html",
+        "jsonls",
+        "taplo",
+        "yamlls",
+        -- Latex
+        "ltex",
+        -- Markdown
+        "marksman",
+        -- Python
+        "pyright",
+        "pylsp",
+        -- Terraform
+        "terraformls",
+        -- Ansible
+        "ansiblels",
+        -- Kubernetes
+        "helm_ls",
+    },
+})
 
 lsp.setup()
 
@@ -13,15 +45,15 @@ local cmp = require('cmp')
 local cmp_action = require('lsp-zero').cmp_action()
 
 cmp.setup({
-  mapping = {
-    -- `Enter` key to confirm completion
-    ['<CR>'] = cmp.mapping.confirm({select = false}),
+    mapping = {
+        -- `Enter` key to confirm completion
+        ['<CR>'] = cmp.mapping.confirm({select = false}),
 
-    -- Ctrl+Space to trigger completion menu
-    ['<C-Space>'] = cmp.mapping.complete(),
+        -- Ctrl+Space to trigger completion menu
+        ['<C-Space>'] = cmp.mapping.complete(),
 
-    -- Navigate between snippet placeholder
-    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
-  }
+        -- Navigate between snippet placeholder
+        ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+        ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+    }
 })
