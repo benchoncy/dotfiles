3b8b869fdea94646047df878b4a8027c21a84e56
Ben Stuart <ben@benstuart.ie>
2023-08-11 23:58:26 +0100

    Refactor nvim filetypes

--- dot_config/nvim/lua/exact_config/filetypes.lua
+++ dot_config/nvim/lua/exact_config/filetypes.lua
@@ -1,26 +1,31 @@
 -- Add or override filetype mappings
 
+local function ft_yaml(path, bufnr)
+    print(path)
+    -- Match ansible file paths
+    if path:match('tasks/.+%.y(a?)ml') or
+            path:match('handlers/.+%.y(a?)ml') or
+            path:match('roles/.+%.y(a?)ml') then
+        return 'yaml.ansible'
+    end
+
+    -- Match ansible content
+    local content = vim.filetype.getlines(bufnr)
+    if type(content) == "table" then
+        content = table.concat(content, "\n")
+    end
+    if content:match('hosts.*gather_facts') or
+            content:match('ansible%.builtin') then
+        return 'yaml.ansible'
+    end
+
+    -- Default to yaml
+    return 'yaml'
+end
+
 vim.filetype.add({
-    extentions = {
-      tf = 'terraform',
-    },
-    pattern = {
-        -- YAML files
-        ['.*%.y(a?)ml'] = function(path, bufnr)
-            -- Get file content
-            local content = vim.filetype.getlines(bufnr)
-            if type(content) == "table" then
-                content = table.concat(content, "\n")
-            end
-            -- Check if file is ansible, based on path or content
-            if path:match('tasks/.+%.y(a?)ml') or
-               path:match('handlers/.+%.y(a?)ml') or
-               path:match('roles/.+%.y(a?)ml') or
-               content:match('ansible%.builtin') then
-                return 'yaml.ansible'
-            end
-            -- Default to yaml
-            return 'yaml'
-        end,
+    extension = {
+        yaml = ft_yaml,
+        yml = ft_yaml,
     },
 })
