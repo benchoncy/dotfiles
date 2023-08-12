23270dd38653272fa0c7a311be259ba46b2735ba
Ben Stuart <ben@benstuart.ie>
2023-08-12 18:37:52 +0100

    Add lsp status to lualine

--- dot_config/nvim/lua/exact_plugins/lsp-zero.lua
+++ dot_config/nvim/lua/exact_plugins/lsp-zero.lua
@@ -4,7 +4,7 @@
 local config = function()
     local lsp = require('lsp-zero').preset({})
 
-    lsp.on_attach(function(client, bufnr)
+    lsp.on_attach(function(_, bufnr)
         lsp.default_keymaps({buffer = bufnr})
     end)
 
--- dot_config/nvim/lua/exact_plugins/lualine.lua
+++ dot_config/nvim/lua/exact_plugins/lualine.lua
@@ -1,15 +1,19 @@
 -- Lualine plugin configuration
 -- Purpose: Provides a configurable statusline UI element.
 
-local config = function()
+local function config()
     require('lualine').setup({
         options = {
             section_separators = {'|', '|'},
             component_separators = {'|', '|'},
         },
+        -- Sections to be shown on the statusline
+        -- In order, left to right, A, B, C, X, Y, Z
+        -- ABC are left sections, XYZ are right sections
+        -- See `:help lualine` for more information
         sections = {
             lualine_x = {'filetype'},
-            lualine_y = {},
+            lualine_y = {'lsp_progress'},
         },
     })
 end
@@ -18,6 +22,7 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = {
         'nvim-tree/nvim-web-devicons',
+        'arkav/lualine-lsp-progress',
     },
     config = config,
 }
