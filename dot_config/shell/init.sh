d2d593ae0e6d8f6af24ba564f3ad917ac29f58be
Ben Stuart <ben@benstuart.ie>
2023-08-24 22:23:21 +0100

    Further cleanup of shell

---
+++ dot_config/shell/init.sh
@@ -0,0 +1,10 @@
+# Initialise shell environment
+
+SHELL_HOME=$HOME/.config/shell
+
+source $SHELL_HOME/general.sh
+source $SHELL_HOME/exports.sh
+source $SHELL_HOME/python.sh
+source $SHELL_HOME/aliases.sh
+source $SHELL_HOME/tmux.sh
+
