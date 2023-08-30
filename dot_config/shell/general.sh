86a3a95ad95efe02f09fec0097bad7a417802a85
Ben Stuart <ben@benstuart.ie>
2023-08-30 21:55:44 +0100

    Add alacritty config

--- dot_config/shell/general.sh
+++
@@ -1,3 +0,0 @@
-# preferred editor
-EDITOR='nvim'
-
--- dot_config/shell/init.sh
+++ dot_config/shell/init.sh
@@ -2,8 +2,6 @@
 
 SHELL_HOME=$HOME/.config/shell
 
-source $SHELL_HOME/general.sh
 source $SHELL_HOME/exports.sh
 source $SHELL_HOME/python.sh
 source $SHELL_HOME/aliases.sh
-source $SHELL_HOME/tmux.sh
