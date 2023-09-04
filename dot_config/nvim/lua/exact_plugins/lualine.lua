c3d5ea6cbea929fde0d3904c63cdfd4f0b008292
Ben Stuart <ben@benstuart.ie>
2023-09-04 23:33:20 +0100

    Style tmux bar to custom style

--- dot_config/nvim/lua/exact_plugins/lualine.lua
+++ dot_config/nvim/lua/exact_plugins/lualine.lua
@@ -10,16 +10,17 @@ return {
     config = function()
         require('lualine').setup({
             options = {
-                section_separators = {'|', '|'},
-                component_separators = {'|', '|'},
+                component_separators = { left = '∕', right = '∕'},
+                section_separators = { left = '', right = ''},
             },
             -- Sections to be shown on the statusline
             -- In order, left to right, A, B, C, X, Y, Z
             -- ABC are left sections, XYZ are right sections
             -- See `:help lualine` for more information
             sections = {
-                lualine_x = {'lsp_progress', 'filetype'},
-                lualine_y = {},
+                lualine_x = {'lsp_progress'},
+                lualine_y = {'filetype'},
+                lualine_z = {'location'},
             },
         })
     end,
