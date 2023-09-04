b51bb1c0f62c4ff4167ce4526976289f5c5a8c8d
Ben Stuart <ben@benstuart.ie>
2023-09-04 21:45:56 +0100

    Add alacritty config

--- dot_config/nvim/lua/exact_plugins/theme.lua
+++ dot_config/nvim/lua/exact_plugins/theme.lua
@@ -5,7 +5,7 @@ return {
     "navarasu/onedark.nvim",
     config = function ()
         require("onedark").setup({
-            style = "warmer"
+            style = "warmer",
         })
         vim.cmd.colorscheme "onedark"
     end
