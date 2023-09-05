9bd0c12da57131d43bf7eda3fb378538267c6069
Ben Stuart <ben@benstuart.ie>
2023-09-05 11:12:00 +0100

    Fix term

--- dot_config/shell/init.sh
+++ dot_config/shell/init.sh
@@ -5,3 +5,13 @@ SHELL_HOME=$HOME/.config/shell
 source $SHELL_HOME/exports.sh
 source $SHELL_HOME/python.sh
 source $SHELL_HOME/aliases.sh
+
+# Start tmux
+if command -v tmux &> /dev/null && \
+   [ -n "$PS1" ] && \
+   [[ ! "$TERM" =~ screen ]] && \
+   [[ ! "$TERM" =~ tmux ]] && \
+   [ -z "$TMUX" ]
+then
+    exec tmux new-session -A -D -s default
+fi
