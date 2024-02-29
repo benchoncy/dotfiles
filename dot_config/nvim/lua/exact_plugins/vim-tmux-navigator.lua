a12db811e0db10a8196e7905028a77b3b934db9e
Ben Stuart <ben@benstuart.ie>
2024-02-29 22:07:41 +0000

    Remove cron and update general config

---
+++ dot_config/nvim/lua/exact_plugins/vim-tmux-navigator.lua
@@ -0,0 +1,20 @@
+-- Vim Tmux navigation integration.
+-- Purpose: Allows vim-tmux seemless pane navigation.
+
+return {
+  "christoomey/vim-tmux-navigator",
+  cmd = {
+    "TmuxNavigateLeft",
+    "TmuxNavigateDown",
+    "TmuxNavigateUp",
+    "TmuxNavigateRight",
+    "TmuxNavigatePrevious",
+  },
+  keys = {
+    { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+    { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+    { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+    { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+    { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+  },
+}
--- run_after_configure_crontab.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# This script is used to configure crontab for the user
-
-CRONTAB_FILE="$HOME/.crontab.cron"
-crontab < $CRONTAB_FILE
