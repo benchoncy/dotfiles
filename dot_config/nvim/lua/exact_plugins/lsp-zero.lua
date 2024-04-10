a9913ffe467649e01501f0707c8eec0ba3e69093
Ben Stuart <ben@benstuart.ie>
2024-04-10 19:44:59 +0100

    Set CR as cmp select key

--- dot_config/nvim/lua/exact_plugins/lsp-zero.lua
+++ dot_config/nvim/lua/exact_plugins/lsp-zero.lua
@@ -61,6 +61,7 @@ return {
             ['rust_analyzer'] = {'rust'},
             ['gopls'] = {'go'},
             ['ruff_lsp'] = {'python'},
+            ['bashls'] = {'bash'},
             ['terraformls'] = {'terraform'},
           }
         })
@@ -102,7 +103,7 @@ return {
             mapping = cmp.mapping.preset.insert({
                 ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
                 ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                ['<CR>'] = cmp.mapping.confirm({ select = true }),
                 ['<C-Space>'] = cmp.mapping.complete(),
             }),
             snippet = {
