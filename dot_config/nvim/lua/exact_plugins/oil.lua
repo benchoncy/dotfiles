bc43c09b126cb7696a56968386e6d3b53af96d1a
Ben Stuart <ben@benstuart.ie>
2024-05-17 00:00:13 +0100

    Fix multigit setup

--- dot_config/nvim/lua/exact_plugins/oil.lua
+++ dot_config/nvim/lua/exact_plugins/oil.lua
@@ -5,4 +5,11 @@ return {
     'stevearc/oil.nvim',
     opts = {},
     dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        require("oil").setup({
+            view_options = {
+                show_hidden = true,
+            },
+        })
+    end,
 }
