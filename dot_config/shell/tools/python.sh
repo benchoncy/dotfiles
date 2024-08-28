b60017f516619f2dbedcc284567d915d294504b9
Ben Stuart <ben@benstuart.ie>
2024-08-28 14:21:56 +0100

    Edit git config, python pyenv, terraform nvim - Enable git config for commit signing - Fix python pyenv config - Add terraform nvim config

--- dot_config/shell/tools/python.sh
+++ dot_config/shell/tools/python.sh
@@ -2,8 +2,8 @@
 
 # pyenv
 export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init --path)"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
 
 # conda
 conda_init() {
