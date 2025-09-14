3790a871a222e4dc8850b9039fcb03de5e5a1571
Ben Stuart <ben@benstuart.ie>
2025-09-14 19:52:31 +0100

    nvim - disable arrow keys in all modes

--- dot_config/nvim/lua/exact_config/remap.lua
+++ dot_config/nvim/lua/exact_config/remap.lua
@@ -14,6 +14,13 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to prev diagnos
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>xs', vim.diagnostic.open_float, { desc = 'Show diagnostic error messages' })
 vim.keymap.set('n', '<leader>xq', vim.diagnostic.setloclist, { desc = 'Open diagnostic quickfix list' })
+
 -- Buffer keymaps
 vim.keymap.set('n', '[b', ':bprev<CR>', { desc = 'Prev buffer' })
 vim.keymap.set('n', ']b', ':bnext<CR>', { desc = 'Next buffer' })
+
+-- Disable arrow keys in all modes
+vim.keymap.set({'n', 'i', 'v'}, '<Up>', '<Nop>')
+vim.keymap.set({'n', 'i', 'v'}, '<Down>', '<Noop>')
+vim.keymap.set({'n', 'i', 'v'}, '<Left>', '<Noop>')
+vim.keymap.set({'n', 'i', 'v'}, '<Right>', '<Noop>')
